8ad3bdcb63b710bd2247f0f412751b8990a514ec
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-04-09 09:32:12 +0200

    add queue

--- stacks/stacks/Source.cpp
+++ stacks/stacks/Source.cpp
@@ -3,67 +3,113 @@
 using namespace std;
 
 #define SIZE (6)
-class Stack
-{
-public:
-	// Ctor. and Dtor.
-	Stack() 
-	{ 
-		_array = new int[SIZE](); 
-		_top = -1;
-	}
-
-	~Stack() 
+//class Stack
+//{
+//public:
+//	// Ctor. and Dtor.
+//	Stack() 
+//	{ 
+//		_array = new int[SIZE](); 
+//		_top = -1;
+//	}
+//
+//	~Stack() 
+//	{
+//		delete[] _array;
+//	}
+//	// Modifiers
+//
+//	void push(int value)
+//	{ 
+//		if (_top + 1 < SIZE)
+//		{
+//			_array[_top + 1] = value;
+//			_top++;
+//		}
+//		//else /*(_top+1 == SIZE)*/
+//		//{
+//		//	_top = -1;
+//		//	_array[_top + 1] = value;
+//		//}
+//	}
+//	void pop()
+//	{
+//		if (_top > -1)
+//		{
+//			_array[_top] = 0;
+//			_top--;
+//		}
+//	}
+//
+//	// Getters
+//	int top() const
+//	{
+//		return _array[_top];
+//	}
+//	bool empty() const 
+//	{
+//		bool state = false;
+//
+//		if (_top == -1) {
+//			state = true;
+//		}
+//		return state;
+//	}
+//private:
+//	int _top; // Current top index (-1 if empty)
+//	int *_array; // Dynamically allocated array
+//
+//};
+class Queue {
+public:  // Ctor. and Dtor.  
+	Queue() 
 	{
-		delete[] _array;
-	}
-	// Modifiers
+		_array = new int[SIZE];
+		_front = -1;
+		_back = -1;
 
-	void push(int value)
-	{ 
-		if (_top + 1 < SIZE)
+	}
+	~Queue() 
+	{
+		delete[] _array;	
+	}
+	// Modifiers  
+	void enqueue(int value) 
+	{
+		_array[_back+1] = value;
+		_back++;
+		if (_back >= SIZE)
 		{
-			_array[_top + 1] = value;
-			_top++;
-		}
-		//else /*(_top+1 == SIZE)*/
-		//{
-		//	_top = -1;
-		//	_array[_top + 1] = value;
-		//}
-	}
-	void pop()
-	{
-		if (_top > -1)
-		{
-			_array[_top] = 0;
-			_top--;
+			_back = -1;
 		}
 	}
-
-	// Getters
-	int top() const
+	int  dequeue() 
 	{
-		return _array[_top];
+		
+	}
+		 // Getters  
+	int size() const 
+	{
+		int count = _back - _front;
+		if (_back == _front)
+			count = 0;
+		return  count;
 	}
 	bool empty() const 
 	{
-		bool state = false;
-
-		if (_top == -1) {
-			state = true;
-		}
-		return state;
+		bool emp= false;
+		if (_back == _front)
+			emp = true;
+		return true;
 	}
-private:
-	int _top; // Current top index (-1 if empty)
-	int *_array; // Dynamically allocated array
-
-};
+private:  int _front; // Index to the front (-1 if empty)  
+		  int _back; // Index to the back (-1 if empty)  
+		  int *_array; // Dynamically allocated array 
+};
 
 int main() {
 
-	Stack ok;
+	/*Stack ok;
 
 	for (int i = 1; i < SIZE+1; ++i) {
 		ok.push(i);
@@ -85,7 +131,7 @@ int main() {
 	ok.pop();
 
 	cout << " isu is empty? " << ok.empty() << endl;
-	
+	*/
 	system("pause");
 	return 0;
 }
